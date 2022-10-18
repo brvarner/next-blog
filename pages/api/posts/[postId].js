ef2d7fbd4cdf7bf0801883266624504ea2a24d8c
brvarner <bvarner4592@gmail.com>
2022-10-18 10:31:57 -0500

    added JSON stringify to API page to hopefully return proper JSON

--- pages/api/posts/[postId].js
+++ pages/api/posts/[postId].js
@@ -6,7 +6,7 @@ export default function Handler(req, res) {
 
   if (postId) {
     const post = Posts.find((value) => value.id === postId);
-    return res.status(200).json(post);
+    return res.status(200).json(JSON.stringify(post));
   }
 
   return res.status(404).json({ error: "data not found" });
