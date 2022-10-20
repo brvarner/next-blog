12358c142166635b797c2a3e48947d247b8b4751
brvarner <bvarner4592@gmail.com>
2022-10-20 13:46:00 -0500

    changed the way that post loading works to get rid of static props

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -1,27 +1,23 @@
 import { PageContainer } from "../../containers";
 import getPost from "../../lib/helper";
-import Fetcher from "../../lib/fetcher";
-import { Error, Spinner } from "../../components/children";
 import { useRouter } from "next/router";
 import { SWRConfig } from "swr";
 import Head from "next/head";
+import data from "../api/posts/data";
 
 // We use the SWRConfig to cache the data
-export default function Page({ fallback }) {
+export default function Page() {
   const router = useRouter();
   const { postId } = router.query;
-  const { data, isLoading, isError } = Fetcher(`/api/posts/${postId}`);
-
-  if (isLoading) return <Spinner />;
-  if (isError) return <Error />;
+  const postData = data.Posts.find((post) => post.id === postId);
 
   return (
     <>
       <Head>
         <title>Design</title>
       </Head>
-      <SWRConfig value={{ fallback }}>
-        <Article {...data} />
+      <SWRConfig>
+        <Article {...postData} />
       </SWRConfig>
     </>
   );
@@ -38,35 +34,22 @@ function Article({ title, subtitle, image, content }) {
   );
 }
 
-// Get static props is used to grab a specific post from the api on request.
-export async function getStaticProps({ params }) {
-  const posts = await getPost(params.postId);
-
-  return {
-    props: {
-      fallback: {
-        "/api/posts": posts,
-      },
-    },
-  };
-}
-
 // Get static paths is necessary for paths with queries, so that Next.js
 // knows what it's going to render when that query is called.
 // i.e. blog/1 lets Next.js to grab the first one.
-export async function getStaticPaths() {
-  const posts = await getPost();
+// export async function getStaticPaths() {
+//   const posts = await getPost();
 
-  const paths = posts.map((post) => {
-    return {
-      params: {
-        postId: post.id.toString(),
-      },
-    };
-  });
+//   const paths = posts.map((post) => {
+//     return {
+//       params: {
+//         postId: post.id.toString(),
+//       },
+//     };
+//   });
 
-  return {
-    paths,
-    fallback: false,
-  };
-}
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// }
